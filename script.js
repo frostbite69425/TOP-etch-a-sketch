30908b259fee18ae8947e60b9ad633b97f069aff
frostbite69425 <medhihirak3@gmail.com>
2025-04-08 13:38:55 +0530

    feat: add dynamic functions responding to user inp

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
 // selectors
 
-const container = document.body.querySelector("div.container");
-console.log(container);
+const container = document.querySelector("div.container");
+const inputForm = document.querySelector("form.form");
+const input = document.querySelector("input.input-field");
+const submitBtn = document.querySelector(".submit-btn");
+const userAlert = document.querySelector(".wrongInputInfo");
 
-// function for creating grid
-i = 0;
-x = 4;
-y = 4;
-pixMax = 900;
-container.setAttribute("style", `width: ${pixMax}px`);
-for (i; i < x * y; i++) {
-  gridDiv = document.createElement("div");
-  container.appendChild(gridDiv);
-  //   gridDiv.innerText = "hi";
-  gridDiv.classList = "grid grid-border";
-  gridDiv.setAttribute(
-    "style",
-    `min-width: ${pixMax / x}px;
-    min-height: 30px`
-  );
-  //   gridDiv.setAttribute("style", "background-color: black; color: pink");
+// FUNCTION FOR DYNAMICALLY CREATING THE GRID
+
+x = 10;
+pixMaxWidth = 60;
+
+function generateGrid() {
+  container.setAttribute("style", `width: ${pixMaxWidth}vw`);
+  for (i = 0; i < x ** 2; i++) {
+    gridDiv = document.createElement("div");
+    container.appendChild(gridDiv);
+    gridDiv.classList = "grid grid-border";
+    gridDiv.setAttribute(
+      "style",
+      `min-width: ${pixMaxWidth / x}vw;
+    min-height:${pixMaxWidth / x}vw`
+    );
+    // gridDiv.innerText = i;
+  }
 }
+generateGrid();
+
+// FUNCTION FOR CHANGING THE GRID DENSITY UPON DETECTING USER INPUT
+
+inputForm.addEventListener("submit", (e) => {
+  x = input.value;
+  console.log(x);
+  e.preventDefault();
+  if (x > 0) {
+    userAlert.innerHTML = "";
+    container.innerHTML = "";
+    generateGrid();
+    inputForm.reset();
+    input.focus();
+  } else {
+    userAlert.textContent = "Please select a number greater than 0";
+  }
+});
